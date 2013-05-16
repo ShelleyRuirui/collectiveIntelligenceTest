39118b2c4757d5063c8126c5ddeb4a0a73ea99f2
Ruirui <081251105@163.com>
2013-05-16 10:25:19 +0800

    Bayes done

--- chap6Filter/docclass.py
+++ chap6Filter/docclass.py
@@ -75,3 +75,49 @@ def sampletrain(cl):
     cl.train('buy pharmaceuticals now','bad')
     cl.train('make quick money at the online casino','bad')
     cl.train('the quick brown fox jumps','good')
+
+
+class naivebayes(classifier):
+
+    def __init__(self,getfeatures):
+        classifier.__init__(self,getfeatures)
+        self.thresholds={}
+    
+    def docprob(self,item,cat):
+        features=self.getfeatures(item);
+
+        #multiply probability all the features together
+        p=1
+        for f in features: p*=self.weightedprob(f,cat,self.fprob)
+        return p
+
+    def prob(self,item,cat):
+        catprob=self.catcount(cat)/self.totalcount()
+        docprob=self.docprob(item,cat)
+        return docprob*catprob
+
+    def setthreshold(self,cat,t):
+        self.thresholds[cat]=t
+
+    def getthreshold(self,cat):
+        if cat not in self.thresholds: return 1.0;
+        return self.thresholds[cat]
+
+    def classify(self,item,default=None):
+        probs={}
+        #Find the category with the highest probability
+        max=0.0
+        best=default
+        for cat in self.categories():
+            probs[cat]=self.prob(item,cat)
+            if probs[cat]>max:
+                max=probs[cat]
+                best=cat
+
+        #Make sure the probability exceeds threshold*next best
+        for cat in probs:
+            if cat==best: continue
+            if probs[cat]*self.getthreshold(best)>probs[best]: return default
+
+        return best
+    
